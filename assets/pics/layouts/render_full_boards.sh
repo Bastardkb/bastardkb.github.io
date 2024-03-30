85e7b351515d5d267e309e1887efc46bd2e1719d
burkfers <finrod09@users.noreply.github.com>
2024-03-30 14:59:48 +0100

    improve layout rendering script

--- assets/pics/layouts/render_full_boards.sh
+++ assets/pics/layouts/render_full_boards.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-#keymap draw cnano.yaml > cnano.svg && convert -size 800x800 -background '#303030' cnano.svg cnano.png && feh cnano.png
+ensure_exists_in_path() {
+    type $1 >/dev/null 2>&1 || { echo >&2 "Could not find $1 in path. Aborting."; exit 1; }
+}
+
+ensure_exists_in_path keymap
+ensure_exists_in_path convert
 
 for board in cnano cmini charybdis skeletyl tbkmini scylla; do
     echo $board
--- assets/pics/layouts/render_single_layers.sh
+++ assets/pics/layouts/render_single_layers.sh
@@ -1,13 +1,23 @@
 #/bin/bash
 
+ensure_exists_in_path() {
+    type $1 >/dev/null 2>&1 || { echo >&2 "Could not find $1 in path. Aborting."; exit 1; }
+}
+
+ensure_exists_in_path yq
+ensure_exists_in_path tr
+ensure_exists_in_path keymap
+ensure_exists_in_path convert
+
 for board in cnano cmini charybdis skeletyl tbkmini scylla; do
     echo $board
+    rm -r $board
+    mkdir -p $board
     for layer in `yq '.layers | keys[]' $board.yaml | tr -d '"'`; do # parse layer list from yaml, remove quotes
         echo $board/$layer
 
         layer_safe=${layer//\//_} # replace slash with underscore
 
-        mkdir -p $board
         keymap draw -s "$layer" -- $board.yaml > $board/$layer_safe.svg
         convert -size 800x800 -background '#303030' "$board/$layer_safe.svg" "$board/$layer_safe.png"
     done
