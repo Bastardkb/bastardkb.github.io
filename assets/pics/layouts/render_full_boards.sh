2d1a73b128a9da01a1b764f69427accbf0795acf
burkfers <finrod09@users.noreply.github.com>
2024-04-02 16:10:37 +0200

    add layout files for dilemma v2

--- assets/pics/layouts/render_full_boards.sh
+++ assets/pics/layouts/render_full_boards.sh
@@ -7,7 +7,7 @@ ensure_exists_in_path() {
 ensure_exists_in_path keymap
 ensure_exists_in_path convert
 
-for board in cnano cmini charybdis skeletyl tbkmini scylla; do
+for board in cnano cmini charybdis skeletyl tbkmini scylla dilemma; do
     echo $board
     keymap draw $board.yaml > $board.svg
     convert -size 800x800 -background '#303030' $board.svg $board.png
