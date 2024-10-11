4913252e4c58dfc3963f69fb01cca1dcc18a9255
burkfers <6268175+burkfers@users.noreply.github.com>
2024-10-11 17:09:59 +0200

    layout image renderer: change deprecated way of calling imagemagick

--- assets/pics/layouts/render_full_boards.sh
+++ assets/pics/layouts/render_full_boards.sh
@@ -5,11 +5,11 @@ ensure_exists_in_path() {
 }
 
 ensure_exists_in_path keymap
-ensure_exists_in_path convert
+ensure_exists_in_path magick
 
 for board in cnano cmini charybdis skeletyl tbkmini scylla dilemma dilemma_max; do
     echo $board
     keymap draw $board.yaml > $board.svg
-    convert -size 800x800 -background '#303030' $board.svg $board.png
+    magick -size 800x800 -background '#303030' $board.svg $board.png
 done
 echo done!
--- assets/pics/layouts/render_single_layers.sh
+++ assets/pics/layouts/render_single_layers.sh
@@ -7,7 +7,7 @@ ensure_exists_in_path() {
 ensure_exists_in_path yq
 ensure_exists_in_path tr
 ensure_exists_in_path keymap
-ensure_exists_in_path convert
+ensure_exists_in_path magick
 
 for board in cnano cmini charybdis skeletyl tbkmini scylla dilemma dilemma_max; do
     echo $board
@@ -19,6 +19,6 @@ for board in cnano cmini charybdis skeletyl tbkmini scylla dilemma dilemma_max; 
         layer_safe=${layer//\//_} # replace slash with underscore
 
         keymap draw -s "$layer" -- $board.yaml > $board/$layer_safe.svg
-        convert -size 800x800 -background '#303030' "$board/$layer_safe.svg" "$board/$layer_safe.png"
+        magick -size 800x800 -background '#303030' "$board/$layer_safe.svg" "$board/$layer_safe.png"
     done
 done
